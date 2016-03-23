1dc5063aac7b628c5c652eb5d35678428b5bf22c
Phil Barker <phil@pjjk.net>
2016-03-23 18:06:26 +0000

    towards posts_by_taxon...

--- macs-taxonomies.php
+++ macs-taxonomies.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * Taxonmies used for the MACS students pages
- *
+ * i.e.: location, department, level, delivery level
+ * posts_by_taxon() : return list of posts as classified by these txnms
  */
 
 function location_init() 
@@ -57,7 +58,7 @@ function level_init()
 		'label'=> 'SCQA Level',
 		'hierarchical' => true,
 		'show_ui' => true,
-		'show_in_menu' => false,
+		'show_in_menu' => true,
 		'show_in_nav_menus' => true		
 	);
 	register_taxonomy( $name, $object_type, $args);
@@ -66,7 +67,7 @@ add_action( 'init', 'level_init' );
 
 function delivery_level_init() 
 {
-	$name ='deliveryLevels';
+	$name ='deliveryLevel';
 	$object_type = 'course';
 	$args = array(
 		'label'=> 'Delivery Levels',
@@ -79,3 +80,66 @@ function delivery_level_init()
 }
 add_action( 'init', 'delivery_level_init' );
 
+function posts_by_taxon ( $atts ) 
+{
+	$results = '<p>Posts by taxon: ';
+	$query = array();
+	$query['posts_per_page'] = 5;
+	$query['post_type'] = $atts['type'];
+	$query['tax_query'] = array();
+
+	if ( array_key_exists( 'level', $atts ) )
+	{
+		$results = $results.'level = '.$atts['level'];
+		$level_tax_query = array(
+			'taxonomy' => 'level',
+			'field' => 'name',
+			'terms' => $atts['level']
+		);
+		$query['tax_query'][] = $level_tax_query;
+	}
+	if ( array_key_exists( 'semester', $atts ) )
+	{
+		$results = $results.'semester = '.$atts['semester'];
+		$semester_tax_query = array(
+			'taxonomy' => 'semester',
+			'field' => 'name',
+			'terms' => $atts['semester']
+		);
+		$query['tax_query'][] = $semester_tax_query;
+	}
+	if ( array_key_exists( 'department', $atts ) )
+	{
+		$results = $results.' department = '.$atts['department'];
+		$department_tax_query = array(
+			'taxonomy' => 'department',
+			'field' => 'name',
+			'terms' => $atts['department']
+		);
+		$query['tax_query'][] = $department_tax_query;
+	}
+	if ( array_key_exists( 'delivery_level', $atts ) )
+	{
+		$results = $results.' department = '.$atts['department'];
+		$department_tax_query = array(
+			'taxonomy' => 'department',
+			'field' => 'name',
+			'terms' => $atts['department']
+		);
+		$query['tax_query'][] = $department_tax_query;
+	}
+
+	$posts_array = get_posts( $query );
+
+	$results = $results.'</p><ul>';
+	foreach ( $posts_array as $post ) 
+	{
+		$url = esc_url( get_permalink( $post->ID ) );
+		$title = $post->post_title;
+		$linkitem = '<li><a href="'.$url.'">'.$title.'</a></li>';
+		$results = $results.$linkitem;
+	}
+	$results = $results.'</ul>';
+	return $results;
+}
+add_shortcode( 'posts-by-taxon', 'posts_by_taxon' );
